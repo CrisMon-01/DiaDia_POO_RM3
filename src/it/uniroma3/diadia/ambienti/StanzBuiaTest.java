6987d54a8c8087269cd13870d7084a36e4bc8a65
Cristian Monti <Cristian Monti@DESKTOP-OA1U12B>
2017-05-16 20:51:34 +0200

    Commit

---
+++ src/it/uniroma3/diadia/ambienti/StanzBuiaTest.java
@@ -0,0 +1,31 @@
+package it.uniroma3.diadia.ambienti;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import it.uniroma3.diadia.attrezzi.Attrezzo;
+
+public class StanzBuiaTest {
+	private StanzaBuia con;
+	private StanzaBuia senza;
+	private Attrezzo luce;
+	@Before
+	public void setUp() throws Exception {
+		luce = new Attrezzo("luce", 1);
+		con = new StanzaBuia("con", "luce");
+		senza = new StanzaBuia("senza", "nonpresente");
+	}
+
+	@Test
+	public void testgetDescrizioneConLuce() {
+		con.addAttrezzo(luce);
+		assertEquals(con.toString(), con.getDescrizione());
+	}
+	@Test
+	public void testgetDescrizioneSenzaLuce() {
+		assertEquals("Qui c' è buio pesto", senza.getDescrizione());
+	}
+
+}
